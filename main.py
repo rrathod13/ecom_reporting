94555f672395908e4bab9ac87cea895ea6e634bb
fubblea <ajay.arthur@gmail.com>
2025-09-27 14:15:12 -0400

    Example with func

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import sqlite3
 
 
+def get_orders(id: str):
+    return f"SELECT * FROM orders WHERE order_id = '{id}';"
+
+
 def main():
     conn = sqlite3.connect("db/ecom_reporting.db")
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM orders LIMIT 5;")
+    sql_str = get_orders("e481f51cbdc54678b7cc49136f2d6af7")
+    print(sql_str)
+
+    print("\nExecuting query...\n")
+
+    cursor.execute(sql_str)
     rows = cursor.fetchall()
 
     for row in rows:
